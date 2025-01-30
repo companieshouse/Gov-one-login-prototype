0795baa5dcff82999ea112c96b43d1a632d09d52
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2025-01-30 16:18:20 +0000

    id choice branch

--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -125,3 +125,155 @@ router.post('/v1/iPhoneModel', function (req, res) {
     res.redirect('/v1/valid-driving-licence')
   }
 })
+
+
+// ******* iPhoneModel javascript ********************************
+router.get('/v1/post-office', function (req, res) {
+  // Set URl
+  res.render('v1/post-office', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/post-office', function (req, res) {
+  if (req.session.data['postOfficeId'] === 'yes')  {
+    res.redirect('/v1/name-on-id')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/bank-questions')
+  }
+})
+
+
+// ******* post-office-id javascript ********************************
+router.get('/v1/post-office-id', function (req, res) {
+  // Set URl
+  res.render('v1/post-office-id', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/post-office-id', function (req, res) {
+  if (req.session.data['postOfficeId'] === 'Passport')  {
+    res.redirect('/v1/passport-expire')
+  } else if (req.session.data['postOfficeId'] === 'Non-UK Passport')  {
+    res.redirect('/v1/does-passport-expire')
+  } else if (req.session.data['postOfficeId'] === 'Driving licence')  {
+    res.redirect('/v1/driving-expire')
+  } else if (req.session.data['postOfficeId'] === 'European driving licence')  {
+    res.redirect('/v1/europe-driving-expire')
+  } else if (req.session.data['postOfficeId'] === 'idCard')  {
+    res.redirect('/v1/eea-id-expire')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/prove-another-way')
+  }
+})
+
+
+// ******* does-passport-expire javascript ********************************
+router.get('/v1/does-passport-expire', function (req, res) {
+  // Set URl
+  res.render('v1/does-passport-expire', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/does-passport-expire', function (req, res) {
+  if (req.session.data['passportHaveExpiry'] === 'expire')  {
+    res.redirect('/v1/passport-expire')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/bank-questions')
+  }
+})
+
+
+// ******* passport-expire javascript ********************************
+router.get('/v1/passport-expire', function (req, res) {
+  // Set URl
+  res.render('v1/passport-expire', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/passport-expire', function (req, res) {
+  if (req.session.data['postOfficeId'] === 'Non-UK Passport') {
+    res.redirect('/v1/select-country')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/find-post-office')
+  }
+})
+
+
+// ******* driving-address javascript ********************************
+router.get('/v1/driving-address', function (req, res) {
+  // Set URl
+  res.render('v1/driving-address', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/driving-address', function (req, res) {
+  if (req.session.data['drivingAddress'] === 'yes')  {
+    res.redirect('/v1/find-post-office')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/post-office-id')
+  }
+})
+
+
+// ******* europe-driving-expire javascript ********************************
+router.get('/v1/europe-driving-expire', function (req, res) {
+  // Set URl
+  res.render('v1/europe-driving-expire', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/europe-driving-expire', function (req, res) {
+  if (req.session.data['europeDriveExpiry'] === 'yes')  {
+    res.redirect('/v1/europe-driving-expire-date')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/europe-driving-address')
+  }
+})
+
+
+// ******* europe-driving-address javascript ********************************
+router.get('/v1/europe-driving-address', function (req, res) {
+  // Set URl
+  res.render('v1/europe-driving-address', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/europe-driving-address', function (req, res) {
+  if (req.session.data['europeDrivingAddress'] === 'yes')  {
+    res.redirect('/v1/europe-driving-country')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/europe-driving-address')
+  }
+})
+
+
+// ******* eea-id-expire javascript ********************************
+router.get('/v1/eea-id-expire', function (req, res) {
+  // Set URl
+  res.render('v1/eea-id-expire', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/eea-id-expire', function (req, res) {
+  if (req.session.data['eeaIdExpiry'] === 'yes')  {
+    res.redirect('/v1/eea-id-expire-date')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/europe-driving-address')
+  }
+})
