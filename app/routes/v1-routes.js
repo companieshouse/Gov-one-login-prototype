fa63d9e287f4669137837b162bf491881007b17b
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2025-02-04 11:02:54 +0000

    finish

--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -51,6 +51,23 @@ router.post('/v1/computer-or-tablet', function (req, res) {
   }
 })
 
+// ******* which-smartphone javascript ********************************
+router.get('/v1/which-smartphone', function (req, res) {
+  // Set URl
+  res.render('v1/which-smartphone', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/which-smartphone', function (req, res) {
+  if (req.session.data['whichSmartphone'] === 'no') {
+    res.redirect('/v1/have-passport-or-licence')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/valid-passport')
+  }
+})
+
 
 // ******* have-a-smartphone javascript ********************************
 router.get('/v1/have-a-smartphone', function (req, res) {
@@ -84,6 +101,26 @@ router.post('/v1/valid-passport', function (req, res) {
   } else {
     // User inputted value so move to next page
     res.redirect('/v1/residence-permit')
+  }
+})
+
+// ******* residence-permit javascript ********************************
+router.get('/v1/residence-permit', function (req, res) {
+  // Set URl
+  res.render('v1/residence-permit', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/residence-permit', function (req, res) {
+  if (req.session.data['residencePermit'] === 'no')  {
+    res.redirect('/v1/valid-driving-licence')
+  } else {
+    if (req.session.data['haveSmartphone'] === 'iphone')  {
+      res.redirect('/v1/iphoneModel2')
+    } else if (req.session.data['haveSmartphone'] === 'android')  {
+      res.redirect('/v1/govuk-app-brp')
+    }
   }
 })
 
@@ -123,6 +160,41 @@ router.post('/v1/iPhoneModel', function (req, res) {
   } else {
     // User inputted value so move to next page
     res.redirect('/v1/valid-driving-licence')
+  }
+})
+
+
+// ******* iPhoneModel javascript ********************************
+router.get('/v1/iPhoneModel2', function (req, res) {
+  // Set URl
+  res.render('v1/iPhoneModel2', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/iPhoneModel2', function (req, res) {
+  if (req.session.data['iphoneModel'] === '7')  {
+    res.redirect('/v1/govuk-app-brp')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/valid-driving-licence')
+  }
+})
+
+// ******* valid-driving-licence javascript ********************************
+router.get('/v1/valid-driving-licence', function (req, res) {
+  // Set URl
+  res.render('v1/valid-driving-licence', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/valid-driving-licence', function (req, res) {
+  if (req.session.data['validDrivingLicence'] === 'yes')  {
+    res.redirect('/v1/govuk-app-driving')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/have-passport-or-licence')
   }
 })
 
@@ -274,6 +346,109 @@ router.post('/v1/eea-id-expire', function (req, res) {
     res.redirect('/v1/eea-id-expire-date')
   } else {
     // User inputted value so move to next page
-    res.redirect('/v1/europe-driving-address')
+    res.redirect('/v1/eea-id-address')
   }
+})
+
+
+// ******* eea-id-address javascript ********************************
+router.get('/v1/eea-id-address', function (req, res) {
+  // Set URl
+  res.render('v1/eea-id-address', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/eea-id-address', function (req, res) {
+  if (req.session.data['eeaIdAddress'] === 'no')  {
+    res.redirect('/v1/post-office-id')
+  } else {
+    res.redirect('/v1/eea-id-country')
+  } 
+})
+
+
+// ******* bank-questions javascript ********************************
+router.get('/v1/bank-questions', function (req, res) {
+  // Set URl
+  res.render('v1/bank-questions', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/bank-questions', function (req, res) {
+  if (req.session.data['bankQuestions'] === 'yes')  {
+    res.redirect('/v1/bank-name')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/find-another-way')
+  }
+})
+
+
+// ******* dob javascript ********************************
+router.get('/v1/dob', function (req, res) {
+  // Set URl
+  res.render('v1/dob', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/dob', function (req, res) {
+  if (req.session.data['bankQuestions'] === 'yes')  {
+    res.redirect('/v1/confirm-details-bank')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/confirm-details')
+  }
+})
+
+
+// ******* we-need-to-check javascript ********************************
+router.get('/v1/we-need-to-check', function (req, res) {
+  // Set URl
+  res.render('v1/we-need-to-check', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/we-need-to-check', function (req, res) {
+  if (req.session.data['success'] === 'yes')  {
+    res.redirect('/v1/go-to-service')
+  } else if (req.session.data['bankQuestions'] === 'yes')  {
+    res.redirect('/v1/Answer-security-questions')
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v1/how-to-post-office')
+  }
+})
+
+
+// ******* find-another-way javascript ********************************
+router.get('/v1/find-another-way', function (req, res) {
+  // Set URl
+  res.render('v1/find-another-way', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/find-another-way', function (req, res) {
+  if (req.session.data['anotherWay'] === 'useID')  {
+    res.redirect('/v1/have-id')
+  } else if (req.session.data['anotherWay'] === 'useBankDetails')  {
+    res.redirect('/v1/bank-questions')
+  }
+})
+
+
+// ******* successfully-matched javascript ********************************
+router.get('/v1/successfully-matched', function (req, res) {
+  // Set URl
+  res.render('v1/successfully-matched', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/successfully-matched', function (req, res) {
+  res.redirect('/v1/find-address')
 })
